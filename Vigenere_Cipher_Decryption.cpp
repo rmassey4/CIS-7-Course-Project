f4203cf7e792dd5b2829a49da85654ee6bfa7ff7
rmassey4 <162090940+rmassey4@users.noreply.github.com>
2024-11-21 22:28:10 -0800

    Update Vigenere_Cipher_Decryption.cpp
    fixed case bug, need to fix the encryption/decryption

--- Vigenere_Cipher_Decryption.cpp
+++ Vigenere_Cipher_Decryption.cpp
@@ -26,6 +26,18 @@ std::string encrypted_message (std::string text, std::string keyword){
 //function for vigenere cipher decryption
 std::string decrypted_message(std::string text2, std::string keyword){
     std::string decrypted_text = "";
+    int keyword_index = 0;
+    for (int i = 0; i < text2.length(); i++){
+        if(isalpha(text2[i])){
+            char decrypted_char = 'a' + ((text2[i] - 'a' - (keyword[keyword_index] + 26) % 26));
+            decrypted_text += decrypted_char;
+            keyword_index++;
+        }
+        else {
+            decrypted_text += text2[i];
+        }
+    }
+    return decrypted_text;
 }
 
 
@@ -45,6 +57,7 @@ int main() {
         if (chosen_number >= 1 && chosen_number <= 3) {
             switch (chosen_number) {
                 case 1:        //encrypt message
+                    {
                     std::cout << "Please enter the message you would like to encrypt.\n";
                     std::cin >> message;
                     std::cout << "Please enter the key/keyword.\n";
@@ -52,8 +65,9 @@ int main() {
                     std::string encrypted_text = encrypted_message(message, keyword);
                     std::cout << "Encrypted message: " << encrypted_text << std::endl;
                     break;
-                
+                    }
                 case 2:        //decrypt message
+                    {
                     std::cout << "Please enter the message you would like to decrypt.\n";
                     std::cin >> message;
                     std::cout << "Please enter the key/keyword.\n";
@@ -61,14 +75,17 @@ int main() {
                     std::string decrypted_text = decrypted_message(message, keyword);
                     std::cout << "Decrypted message: " << decrypted_text << std::endl;
                     break;
+                    }
                 
                 case 3:        //exit the program
+                    {
                   std::cout << "Goodbye, thank you for using the Encryption/Decryption program!\n";
                     break;
+                    }
             }
         } 
         else {                //exception handling for numbers outside 1-3
-            std::cout << "Please try again, entering a number between 1-3.\n";
+            std::cout << "Please try again, try entering a number between 1-3.\n";
         }
     }
     while (chosen_number != 3);
